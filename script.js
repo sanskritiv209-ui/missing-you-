7533fb1f50607f67cb3d0f085ebaaec93ed0444b
sanskritiv209-ui <sanskritiv209@gmail.com>
2026-07-04 11:30:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+const messages=[
+"🥺 Sunte Ho...",
+"❤️ Are Suno To Sahi...",
+"💌 Ek Baat Batani Thi Aapko...",
+"😔 Main Aapko Bahut Jyada Miss Kar Rahi Hu...",
+"🧸 Give Me A Hug...",
+"❤️ Come Soon... I Need You..."
+];
+
+let current=0;
+
+function openGift(){
+
+document.getElementById("giftBox").style.display="none";
+
+document.getElementById("messageBox").style.display="block";
+
+typeWriter(messages[current]);
+
+createHearts();
+
+}
+
+function nextText(){
+
+current++;
+
+if(current<messages.length){
+
+typeWriter(messages[current]);
+
+}
+
+if(current==4){
+
+document.getElementById("teddy").style.display="block";
+
+}
+
+if(current>=messages.length-1){
+
+document.getElementById("nextBtn").style.display="none";
+
+}
+
+}
+
+function typeWriter(text){
+
+let i=0;
+
+const target=document.getElementById("typing");
+
+target.innerHTML="";
+
+document.getElementById("nextBtn").style.display="none";
+
+const timer=setInterval(()=>{
+
+target.innerHTML+=text.charAt(i);
+
+i++;
+
+if(i>=text.length){
+
+clearInterval(timer);
+
+document.getElementById("nextBtn").style.display="inline-block";
+
+}
+
+},70);
+
+}
